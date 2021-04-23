c8fcaf5b2cc7e35d92d9c7cab78d1e06561c227a
vm <pr701@protonmail.com>
2021-04-23 03:21:58 +0300

    add license

--- include/idb3.hpp
+++ include/idb3.hpp
@@ -1,12 +1,19 @@
 /*
- * idb3.h is a library for accessing IDA Pro databases.
+ * idb3.hpp is a header-library for accessing IDA Pro databases.
  *
  * Author: Willem Hengeveld <itsme@xs4all.nl>
  * Contributed: Vitaly Maslov <pr701@protonmail.com>
  *
  * Toplevel class: IDBFile, use getsection to get a stream to the desired section,
  * Then create an ID0File, ID1File, NAMFile for that section.
- * 
+ 
+ * THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 #include <iostream>
 #include <sstream>
